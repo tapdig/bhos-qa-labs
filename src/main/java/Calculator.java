9a88dfaf5297d95e5b05312e4d7bafe7f8beda44
Tapdig Maharramli <tapdigmm@gmail.com>
2021-10-14 21:26:15 +0400

    Lab4

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return a / b;
+    }
+
+    public double squareRoot(double a) {
+        if (a >= 0)
+            return Math.sqrt(a);
+        return 0;
+    }
+
+    public boolean isOdd(int a) {
+        return a % 2 != 0;
+    }
+
+    public boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    public double logarithm(double a) {
+        return Math.log(a);
+    }
+
+    public double findPowerOfBaseToExponent(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public void startCalculator() throws IOException {
+        Runtime.getRuntime().exec("calc.exe");
+    }
+}
