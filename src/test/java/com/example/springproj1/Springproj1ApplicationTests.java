b55094d46107c2a41d6b09970d65e338803bb87b
Tapdig Maharramli <tapdigmm@gmail.com>
2021-10-21 16:19:05 +0400

    updated tests checking for successful json response and status code

--- src/test/java/com/example/springproj1/Springproj1ApplicationTests.java
+++ src/test/java/com/example/springproj1/Springproj1ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.springproj1;
 
+import org.json.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -30,6 +32,19 @@ class Springproj1ApplicationTests {
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
+    public boolean isResponseJSON(String response) {
+        try {
+            new JSONObject(response);
+        } catch (JSONException e) {
+            try {
+                new JSONArray(response);
+            } catch (JSONException ex) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @DisplayName("Integration Test for the first endpoint")
     @Test
     public void testSuccessfulResponseRepos() {
@@ -37,6 +52,7 @@ class Springproj1ApplicationTests {
         ResponseEntity<String> response = restTemplate.getForEntity("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos", String.class);
 
         assertThat(response).isNotNull();
+        assertTrue(isResponseJSON(response.getBody()));
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
@@ -47,6 +63,7 @@ class Springproj1ApplicationTests {
         ResponseEntity<String> response = restTemplate.getForEntity("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches", String.class);
 
         assertThat(response).isNotNull();
+        assertTrue(isResponseJSON(response.getBody()));
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
@@ -57,6 +74,7 @@ class Springproj1ApplicationTests {
         ResponseEntity<String> response = restTemplate.getForEntity("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches/1/commits", String.class);
 
         assertThat(response).isNotNull();
+        assertTrue(isResponseJSON(response.getBody()));
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
