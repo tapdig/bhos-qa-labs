413dde860b42a0398a183aa7a2f5842a677a3a1f
Tapdig Maharramli <tapdigmm@gmail.com>
2021-10-21 15:55:13 +0400

    updated tests

--- src/test/java/com/example/springproj1/Springproj1ApplicationTests.java
+++ src/test/java/com/example/springproj1/Springproj1ApplicationTests.java
@@ -1,17 +1,12 @@
 package com.example.springproj1;
 
-import netscape.javascript.JSObject;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
