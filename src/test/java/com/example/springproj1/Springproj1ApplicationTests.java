95fd41636e1604b93fd9c5105ae0352150efad9f
Tapdig Maharramli <tapdigmm@gmail.com>
2021-10-21 15:53:55 +0400

    updated tests

--- src/test/java/com/example/springproj1/Springproj1ApplicationTests.java
+++ src/test/java/com/example/springproj1/Springproj1ApplicationTests.java
@@ -1,11 +1,18 @@
 package com.example.springproj1;
 
+import netscape.javascript.JSObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -31,21 +38,30 @@ class Springproj1ApplicationTests {
     @DisplayName("Integration Test for the first endpoint")
     @Test
     public void testSuccessfulResponseRepos() {
-        String result = new RestTemplate().getForObject("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos", String.class);
-        assertNotNull(result);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos", String.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @DisplayName("Integration Test for the second endpoint")
     @Test
     public void testSuccessfulResponseBranches() {
-        String result = new RestTemplate().getForObject("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches", String.class);
-        assertNotNull(result);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches", String.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @DisplayName("Integration Test for the third endpoint")
     @Test
     public void testSuccessfulResponseCommits() {
-        String result = new RestTemplate().getForObject("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches/1/commits", String.class);
-        assertNotNull(result);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity("https://60a21d3f745cd70017576092.mockapi.io/api/v1/repos/1/branches/1/commits", String.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
