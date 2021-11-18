e972a18bfdbfa6f5bdaadb466460b462e827ee28
Tapdig Maharramli <tapdigmm@gmail.com>
2021-11-18 21:38:25 +0400

    LAB9

---
+++ src/main/java/com/example/springproj5/User.java
@@ -0,0 +1,15 @@
+package com.example.springproj5;
+
+public class User {
+    public String name;
+    public String surname;
+    public String phone;
+
+    public User(String name, String surname, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+    public User(){}
+}
