d67bee3b0c99f0fa4af34724ae3e942488015aac
Tapdig Maharramli <tapdigmm@gmail.com>
2021-11-29 20:46:44 +0400

    LAB10

---
+++ src/main/java/com/example/springproj6/User.java
@@ -0,0 +1,15 @@
+package com.example.springproj6;
+
+public class User {
+    public String name;
+    public String surname;
+    public String phone;
+
+    public User(String name, String surname, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+    public User(){}
+}
---
+++ src/test/java/com/example/springproj6/Springproj6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproj6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproj6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
