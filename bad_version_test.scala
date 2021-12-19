abdc80720488bdfec4a6007a5731dd20ad19b6a8
Tapdig Maharramli <tapdigmm@gmail.com>
2021-12-19 23:56:26 +0400

    LAB17

---
+++ bad_version_test.scala
@@ -0,0 +1,29 @@
+package BadVersionTest
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BadVersionSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0")
+
+
+  val scn = scenario("BadVersionTest")
+    .exec(
+      http("BadVersion")
+        .post("/isbadversionlinear")    // Linear Search Test
+        // .post("/isbadversionbinary")      // Binary Search Test
+        .body(RawFileBody("input_data.txt"))
+    )
+
+  setUp(scn.inject(
+    atOnceUsers(1)
+  ).protocols(httpProtocol))
+}
---
+++ report.md
@@ -0,0 +1,3 @@
+# Individual Analysis Report
+
+Results show that Linear Search found First Bad Version in 437 ms and Binary Search found in 133 ms. This is because Linear Search (O(n)) goes over all elements, whereas Binary Search (O(log(n))) eliminates half of the elements in each iteration.
