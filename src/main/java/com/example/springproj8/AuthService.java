579d63dad5a2f86b8bb669024b3560b34d5c2f26
Tapdig Maharramli <tapdigmm@gmail.com>
2021-12-19 18:51:17 +0400

    LAB13

--- src/main/java/com/example/springproj8/AuthService.java
+++ src/main/java/com/example/springproj8/AuthService.java
@@ -1,11 +1,12 @@
 package com.example.springproj8;
 
-import com.nimbusds.srp6.SRP6CryptoParams;
-import com.nimbusds.srp6.SRP6Exception;
-import com.nimbusds.srp6.SRP6ServerSession;
+import com.nimbusds.srp6.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigInteger;
 
@@ -37,6 +38,37 @@ public class AuthService {
             return serverSession.step2(computeValuesReqBody.A, computeValuesReqBody.M1).toString();
         } catch (SRP6Exception e) {
             return "";
+        }
+    }
+
+    RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+
+    @GetMapping("/srpLoadTest")
+    public ResponseEntity<Object> srpLoadTest() throws SRP6Exception {
+        SRP6ClientSession Client = new SRP6ClientSession();
+        String username = "test_user";
+        String password = "notsostrongpassword";
+        Client.step1(username, password);
+
+        HttpEntity<String> newSesEntity = new HttpEntity<>(username, headers);
+        String response = restTemplate.exchange("http://localhost:8080/step1", HttpMethod.POST, newSesEntity, String.class).getBody();
+
+        BigInteger serverPublicB = new BigInteger(response);
+
+        SRP6CryptoParams defaultCryptoParams = SRP6CryptoParams.getInstance();
+        BigInteger saltS = new BigInteger(salt);
+        SRP6ClientCredentials clientPublicAEvidenceM1 = Client.step2(defaultCryptoParams, saltS, serverPublicB);
+
+        ComputeValuesReqBody compValBody = new ComputeValuesReqBody(clientPublicAEvidenceM1.A, clientPublicAEvidenceM1.M1);
+        HttpEntity<ComputeValuesReqBody> compValEntity = new HttpEntity<>(compValBody, headers);
+
+        String serverEvidenceM2 = restTemplate.postForEntity("http://localhost:8080/step2", compValEntity, String.class).getBody();
+
+        if (serverEvidenceM2 == "") {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(serverEvidenceM2);
         }
     }
 }
